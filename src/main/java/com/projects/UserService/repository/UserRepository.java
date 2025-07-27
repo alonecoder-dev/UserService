11c8218a676d5805714a208a22c0c22eaed796c3
alonecoder-dev <teja.vallabhadasu@gmail.com>
2025-07-27 18:25:56 +0530

    Completed UserService

---
+++ src/main/java/com/projects/UserService/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.projects.UserService.repository;
+
+import com.projects.UserService.entities.UserInfo;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface UserRepository extends CrudRepository<UserInfo, String>
+{
+
+    Optional<UserInfo> findByUserId(String userId);
+
+}
